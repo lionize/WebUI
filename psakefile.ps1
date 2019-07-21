70d0698ac6b193fcbc04fb4e91d41eb6c460bf80
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-07-21 09:51:35 +0300

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,28 +1,35 @@
 Task Publish -Depends Pack {
-   Exec { docker login docker.io  --username=ashotnazaryan45 }
-   $remoteTag = "docker.io/$script:latestImageTag"
-   Exec { docker tag $script:latestImageTag $remoteTag }
-   Exec { docker push $remoteTag }
+    Exec { docker login docker.io  --username=ashotnazaryan45 }
+    $remoteTag = "docker.io/$script:latestImageTag"
+    Exec { docker tag $script:latestImageTag $remoteTag }
+    Exec { docker push $remoteTag }
 }
 
 Task Pack -Depends Build {
-   Exec { docker build -f Dockerfile $script:SourceRootFolder -t $script:latestImageTag }
+    Exec { docker build -f Dockerfile $script:SourceRootFolder -t $script:latestImageTag }
 }
 
-Task Build -Depends Init,Clean {
-   Exec { npm install $script:SourceRootFolder }
+Task Build -Depends Init, Clean {
+    try {
+        Push-Location
+        Set-Location $script:SourceRootFolder
+        Exec { npm install $script:SourceRootFolder }
+    }
+    finally {
+        Pop-Location
+    }
 }
 
 Task Clean -Depends Init {
 }
 
 Task Init {
-   $date = Get-Date
-   $ticks = $date.Ticks
-   $script:latestImageTag = "ashotnazaryan45/lionize-web-ui:latest"
-   $trashFolder = Join-Path -Path . -ChildPath ".trash"
-   $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
-   New-Item -Path $script:trashFolder -ItemType Directory
-   $script:trashFolder = Resolve-Path -Path $script:trashFolder
-   $script:SourceRootFolder = (Resolve-Path ".\ui\").Path
+    $date = Get-Date
+    $ticks = $date.Ticks
+    $script:latestImageTag = "ashotnazaryan45/lionize-web-ui:latest"
+    $trashFolder = Join-Path -Path . -ChildPath ".trash"
+    $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
+    New-Item -Path $script:trashFolder -ItemType Directory
+    $script:trashFolder = Resolve-Path -Path $script:trashFolder
+    $script:SourceRootFolder = (Resolve-Path ".\ui\").Path
 }
