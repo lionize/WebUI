1efaf89e963523540b1226c1740cce5bbfda50c1
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-18 13:02:36 +0300

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -26,14 +26,14 @@ Task Publish -Depends Pack {
 }
 
 Task Pack -Depends CopyArtefacts {
-    Exec { docker build -f Dockerfile $script:artefacts -t $script:latestImageTag }
+    Exec { docker build -f Dockerfile $artefacts -t $script:latestImageTag }
 }
 
 Task CopyArtefacts -Depends Build {
-    $script:artefacts = Join-Path -Path $trashFolder -ChildPath "artefacts"
+    $artefacts = Join-Path -Path $trashFolder -ChildPath "artefacts"
 
-    Copy-Item -Path (Join-Path -Path $SourceRootFolder -ChildPath ".\dist\lionize") -Destination (Join-Path -Path $script:artefacts -ChildPath "build") -Recurse
-    Copy-Item -Path (Join-Path -Path $SourceRootFolder -ChildPath "nginx.conf") -Destination (Join-Path -Path $script:artefacts -ChildPath "nginx.conf")
+    Copy-Item -Path (Join-Path -Path $SourceRootFolder -ChildPath ".\dist\lionize") -Destination (Join-Path -Path $artefacts -ChildPath "build") -Recurse
+    Copy-Item -Path (Join-Path -Path $SourceRootFolder -ChildPath "nginx.conf") -Destination (Join-Path -Path $artefacts -ChildPath "nginx.conf")
 }
 
 Task Build -Depends TranspileModels {
@@ -70,7 +70,7 @@ Task Clean -Depends Init {
 Task Init {
     $date = Get-Date
     $ticks = $date.Ticks
-    $script:imageName = "ashotnazaryan45/lionize-web-ui"
+    $imageName = "ashotnazaryan45/lionize-web-ui"
     $trashFolder = Join-Path -Path . -ChildPath ".trash"
     $trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
     New-Item -Path $trashFolder -ItemType Directory
