be4b8356f3cc414d0ad68dfe7c816a214185933d
Ashot <ashotnazaryan45@outlook.com>
2019-09-09 21:37:45 +0300

    habitica put

--- ui/src/app/shared/utils.ts
+++ ui/src/app/shared/utils.ts
@@ -4,4 +4,12 @@ export class Utils {
         return array.filter((item, index, self) => self.indexOf(item) == index);
     }
 
+    static uniqueArraySet(array) {
+        return Array.from(new Set(array));
+    }
+
+    static recursiveClone(val) {
+        return Array.isArray(val) ? Array.from(val, Utils.recursiveClone(val)) : val;
+    }
+
 }
