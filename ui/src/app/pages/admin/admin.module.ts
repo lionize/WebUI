8e1a45937d53b4f8abd112e5219ae9047dca4a2d
Ashot <ashotnazaryan45@outlook.com>
2019-09-22 11:46:00 +0300

    refactoring

--- ui/src/app/pages/admin/admin.module.ts
+++ ui/src/app/pages/admin/admin.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
 
 import { AdminRoutingModule } from './admin.routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -13,39 +8,17 @@ import { ProvidersService } from './providers/providers.service';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProvidersComponent } from 'src/app/pages/admin/providers/providers.component';
-// TODO move dynamic components to separate module
-import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
-import { PopupComponent } from 'src/app/shared/components/popup/popup.component';
-import { CardComponent } from 'src/app/shared/components/business-components/card/card.component';
-import { ProviderCardComponent } from 'src/app/shared/components/business-components/provider-card/provider-card.component';
-import { LeftMenuComponent } from 'src/app/shared/components/menu/left-menu/left-menu.component';
-import { RightMenuComponent } from 'src/app/shared/components/menu/right-menu/right-menu.component';
-import { HabiticaComponent } from 'src/app/shared/components/dynamic-components/providers/habitica/habitica.component';
-import { MicrosoftComponent } from 'src/app/shared/components/dynamic-components/providers/microsoft/microsoft.component';
-import { GoogleComponent } from 'src/app/shared/components/dynamic-components/providers/google/google.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule, ReactiveFormsModule,
         AdminRoutingModule, 
-        SharedModule,
-        MatButtonModule, MatDialogModule, MatCardModule
+        SharedModule
     ],
     declarations: [
         AdminComponent,
         DashboardComponent,
         ProvidersComponent,
-        DialogComponent,
-        PopupComponent,
-        LeftMenuComponent,
-        RightMenuComponent,
-        CardComponent,
-        ProviderCardComponent,
-        // TODO move dynamic components to separate module
-        HabiticaComponent,
-        MicrosoftComponent,
-        GoogleComponent
     ],
     exports: [
         AdminComponent
@@ -53,14 +26,7 @@ import { GoogleComponent } from 'src/app/shared/components/dynamic-components/pr
     providers: [
         ProvidersService
     ],
-    entryComponents: [
-        // TODO move dynamic components to separate module
-        DialogComponent,
-        PopupComponent,
-        HabiticaComponent,
-        MicrosoftComponent,
-        GoogleComponent,
-    ]
+    entryComponents: []
 })
 
 export class AdminModule { }
